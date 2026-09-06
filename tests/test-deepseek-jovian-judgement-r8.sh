61bc91863fe4a35ddb3870307accafd47bc38044
Martin Vit <martin@voipmonitor.org>
2026-09-06 10:44:36 +0000

    release(ds4): include profiling cleanup guarantee

--- tests/test-deepseek-jovian-judgement-r8.sh
+++ tests/test-deepseek-jovian-judgement-r8.sh
@@ -30,7 +30,7 @@ verify_lock() {
 
 verify_lock vllm \
   7d66922a7bf0c9c7efe9a35a87df128f6b24e762 \
-  8baad5e80636122c540eb659b54a8e89911abb7b \
+  649e4ce042c8380bd56b02578fb3bdd7e7840fef \
   '[628,630,634,553,671,679]'
 verify_lock b12x \
   a1bbd02781c7505754e7aa58a959c1a77891c690 \
@@ -43,13 +43,13 @@ verify_lock lmcache \
 
 output="$(PRINT_RELEASE_CONFIG=1 "${builder}")"
 grep -Fxq 'revision=r8' <<<"${output}"
-grep -Fxq 'vllm_tree=8baad5e80636122c540eb659b54a8e89911abb7b' \
+grep -Fxq 'vllm_tree=649e4ce042c8380bd56b02578fb3bdd7e7840fef' \
   <<<"${output}"
 grep -Fxq 'b12x_tree=e58515a63b7b5d15bbc523258e1e338f49698ce3' \
   <<<"${output}"
 grep -Fxq 'lmcache_tree=86ee2a3bb5675cd3a25b09ad3e2f20dad4720f58' \
   <<<"${output}"
-grep -Fq 'jovian-judgement-vllm8baad5e-b12xe58515a-fi803c466-cu133-torch213-20260906-r8' \
+grep -Fq 'jovian-judgement-vllm649e4ce-b12xe58515a-fi803c466-cu133-torch213-20260906-r8' \
   <<<"${output}"
 
 config="$(docker compose -f "${compose}" config)"
@@ -65,7 +65,7 @@ grep -Fq 'LMCACHE_CHUNK_SIZE: "4096"' <<<"${config}"
 grep -Fq 'LMCACHE_SEPARATE_OBJECT_GROUPS: "1"' <<<"${config}"
 grep -Fq 'LOAD_FORMAT: instanttensor' <<<"${config}"
 grep -Fq 'INSTANTTENSOR_BACKEND: BUFFERED' <<<"${config}"
-grep -Fq 'jovian-judgement-vllm8baad5e-b12xe58515a-fi803c466' <<<"${config}"
+grep -Fq 'jovian-judgement-vllm649e4ce-b12xe58515a-fi803c466' <<<"${config}"
 
 grep -Fq 'ARG LMCACHE_BUILD_VERSION=0.5.2+jj.ds4.r6' \
   "${repo_root}/Dockerfile.deepseek-infernal-invocation-cu133-torch213"
